89607a555218a7d54040b73d5d5402bc37dcbae1
Basavaraj Hadimani <basuhadimani94@gmail.com>
2018-04-17 16:19:27 +0530

    Update indexForm.jsx

--- component/ts/indexForm.jsx
+++ component/ts/indexForm.jsx
@@ -2,6 +2,8 @@ import  React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import  '../styles/style.scss';
 
+var xPos;
+var yPos;
 
 function shape(props) {
     const {ctx, x, y, width, height, name} = props;
@@ -9,7 +11,7 @@ function shape(props) {
     switch(name){
     case "circle" :
                     ctx.beginPath();
-                    ctx.arc(x + 100, y + 100, 50, 0, 2 * Math.PI);
+                    ctx.arc(x , y , 50, 0, 2 * Math.PI);
                     ctx.stroke();
                     ctx.fillStyle = "#996633";
                     ctx.fill();
@@ -22,16 +24,16 @@ function shape(props) {
     case "triangle" :
 
                       ctx.beginPath();
-                      ctx.moveTo(10, 10);
-                      ctx.lineTo(300, 150);
-                      ctx.lineTo(10, 150);
-                      ctx.lineTo(10, 10);
+                      ctx.moveTo(x, y);
+                      ctx.lineTo(x + 200, y + 100);
+                      ctx.lineTo(x, y + 100);
+                      ctx.lineTo(x, y);
                       ctx.stroke();
                       ctx.fillStyle = "#996633";
                       ctx.fill();
                       break;
     case "square" :
-                      ctx.fillRect(x + 100, y  + 100, width , height);
+                      ctx.fillRect(x , y , width , height);
                       ctx.fillStyle="#996633";
                       break;
 
@@ -43,45 +45,92 @@ class IndexForm extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      flag : true,
-      X : 0,
-      Y : 0
+      flag : true
     }
+
   }
 
 
 
-updateCanvas(e) {
-    const ctx = this.refs.canvas.getContext('2d');
+componentDidMount(){
 
-    var canvas = this.refs.canvas;
-    var $canvas=$(".canvas");
-    var canvasOffset=$canvas.offset();
-    var offsetX=canvasOffset.left;
-    var offsetY=canvasOffset.top;
-    var scrollX=$canvas.scrollLeft();
-    var scrollY=$canvas.scrollTop();
+document.getElementById('circle').addEventListener('mouseup', (e)=>{this.updateCanvas(e)});
+document.getElementById('triangle').addEventListener('mouseup', (e)=>{this.updateCanvas(e)});
+document.getElementById('square').addEventListener('mouseup', (e)=>{this.updateCanvas(e)});
+document.getElementById('rectangle').addEventListener('mouseup', (e)=>{this.updateCanvas(e)});
 
-    shape({ctx, x: 50, y: 50, width: 100, height: 100, name : e.target.id});
 
-    function handleMouseDown(e){
-      e.preventDefault();
+$('#circle').draggable(
+    {
+        drag: function(){
+            var offset = $(this).offset();
+             xPos = offset.left;
+             yPos = offset.top;
+            console.log("X : " + xPos+"Y : "+yPos)
+        },
 
-      // get the mouse position
-      var mouseX=parseInt(e.clientX-offsetX);
-      var mouseY=parseInt(e.clientY-offsetY);
+        revert: function(event) {
+          return !event
+          }
+    });
 
-      alert("X :" + mouseX +"Y : " + mouseY);
+$('#triangle').draggable(
+    {
+        drag: function(){
+            var offset = $(this).offset();
+             xPos = offset.left;
+             yPos = offset.top;
+            console.log("X : " + xPos+"Y : "+yPos)
+        },
 
-    }
+        revert: function(event) {
+          return !event
+          }
+    });
+$('#square').draggable(
+    {
+        drag: function(){
+            var offset = $(this).offset();
+             xPos = offset.left;
+             yPos = offset.top;
+            console.log("X : " + xPos+"Y : "+yPos)
+        },
 
-    // listen for mousedown events
-    $(canvas).mousedown(function(e){handleMouseDown(e);})
+        revert: function(event) {
+          return !event
+          }
+    });
+$('#rectangle').draggable(
+    {
+        drag: function(){
+            var offset = $(this).offset();
+             xPos = offset.left;
+             yPos = offset.top;
+            console.log("X : " + xPos+"Y : "+yPos)
+        },
+
+        revert: function(event) {
+          return !event
+          }
+    });
+
 
 
 }
 
 
+updateCanvas(e) {
+
+    const ctx = this.refs.canvas.getContext('2d');
+
+    if(yPos < 150 || yPos > 650){
+      alert("Please drag within the canvas");
+    }else{
+      shape({ctx, x: xPos, y: yPos - 150, width: 100, height: 100, name : e.target.id});
+    }
+  
+
+}
 
 
 hideParent(){
@@ -90,25 +139,22 @@ hideParent(){
 
 
 render() {
-     return (<div onClick={(e)=>{
-     if (e.target.id){
-     this.updateCanvas(e);
-     }
-     }}>
+console.log(this.state);
+     return (<div>
          <canvas ref="canvas" className="canvas" width={1292} height={520}/>
          <div className="box">
          <div className={this.state.flag?"parent":"hideParent"}>
-         <div id="circle" className="ui-widget-header" >
-          Circle
+         <div id="circle"  className="ui-widget-header" >
+            circle
          </div>
          <div  id="triangle" className="ui-widget-header">
-            Triangle
+            triangle
          </div>
          <div  id="square" className="ui-widget-header">
-            Square
+            square
          </div>
          <div  id="rectangle" className="ui-widget-header">
-            Rectangle
+            rectangle
          </div>
               <div className={this.state.flag?"child":"hideChild"} onClick={() => {this.hideParent()}}>
           {this.state.flag?"HIDE>":"<SHOW"}
@@ -119,5 +165,4 @@ render() {
      );
 }
 }
-
 export default IndexForm;
